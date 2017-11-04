cc819a089c0f7c872f82b9a3023a06ccf6358b04
haxoorx <haxoorx@haxoorx>
2017-11-04 21:19:26 +0100

    switched to pygame

--- src/testPygame.py
+++ src/testPygame.py
@@ -1,5 +1,5 @@
 import pygame
-
+import sys
 from pygame.locals import *
 
 pygame.init()
@@ -8,12 +8,25 @@ filename="images/178x162.jpg"
 picture = pygame.image.load(filename)
 picture = pygame.transform.scale(picture, (2280, 720))
 screen = pygame.display.set_mode((640, 480))
+screenMode=0
+
+
 
 # Event loop
 while 1:
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit()
+        if event.type is KEYDOWN:
+            if event.key==K_ESCAPE:
+                sys.exit()
+            if event.key==K_SPACE:
+                if screenMode==0:
+                    pygame.display.set_mode((640, 480), FULLSCREEN, 16)
+                    screenMode=1
+                else:
+                    pygame.display.set_mode((640, 480))
+                    screenMode=0
 
     screen.blit(picture, (0, 0))
     pygame.display.flip()
